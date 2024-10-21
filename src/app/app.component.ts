2955107945ec6e3cec3bfca29649ebf0c499547e
Keith Amundson <amundskm@gmail.com>
2024-10-21 14:20:52 -0500

    Add some starting code

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FeaturesComponent } from './features/features.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MainComponent, FooterComponent, HeaderComponent, FeaturesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'driftless-scour';
+}
