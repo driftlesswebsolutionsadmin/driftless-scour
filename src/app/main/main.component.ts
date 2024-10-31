915baf9c0f4931dbf66aed5c326d43bf7798cbd0
Keith Amundson <amundskm@gmail.com>
2024-10-31 12:52:30 -0500

    Main Page CSS

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from '../product/product.component';
+import { DemoComponent } from '../demo/demo.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, DemoComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
